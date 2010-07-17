2897acf55df6a5f48acec384d51775ae1936421b
Rich Lane <rlane@club.cc.cmu.edu>
2010-07-17 15:52:17 -0700

    lead target

--- orbit.lua
+++ orbit.lua
@@ -18,6 +18,33 @@ function angle_between(x1, y1, x2, y2)
 	return a
 end
 
+-- a*x^2 + b*x + c
+function largest_root_of_quadratic_equation(a, b, c)
+	z = math.sqrt(b^2 - 4*a*c)
+	x1 = (b + z)/(2*a)
+	x2 = (b - z)/(2*a)
+	return math.max(x1, x2)
+end
+
+function lead(x1, y1, x2, y2, vx1, vy1, vx2, vy2, w, t_max)
+	dx = x2 - x1
+	dy = y2 - y1
+	dvx = vx2 - vx1
+	dvy = vy2 - vy1
+	a = (dvx^2 + dvy^2) - w^2
+	b = 2 * (dx*dvx + dy*dvy)
+	c = dx^2 + dy^2
+	t = largest_root_of_quadratic_equation(a, b, c)
+	io.write(string.format("t=%.03g\n", t))
+	if t >= 0 and t <= t_max then
+		x3 = x2 + dvx*t
+		y3 = y2 + dvy*t
+		return angle_between(x1, y1, x3, y3)
+	else
+		return nil
+	end
+end
+
 function distance(x1, y1, x2, y2)
 	return math.sqrt((x2 - x1)^2 + (y2-y1)^2)
 end
@@ -36,9 +63,13 @@ sleep(32)
 
 while true do
 	x, y = position()
+	vx, vy = velocity()
 	a = angle_between(x, y, 0, 0)
 	--io.write(string.format("x=%g y=%g a=%g\n", x, y, a))
 	thrust(a, 20.0/(distance(x, y, 0, 0)^2))
-	fire(a)
+	a2 = lead(x, y, 0, 0, vx, vy, 0, 0, 10, 1)
+	if a2 then
+		fire(a2)
+	end
 	yield()
 end
