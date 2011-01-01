20865c7e8ac8da8b29efe94be1c941a79c973b72
Rich Lane <rlane@club.cc.cmu.edu>
2011-01-01 14:54:11 -0800

    fix determinism test

--- test/test-determinism.sh
+++ test/test-determinism.sh
@@ -3,5 +3,5 @@ export RISC_SEED=$$
 set -e
 for i in `seq 0 16`
 do
-	RISC_NUM_THREADS=$i ./dedicated/risc_dedicated scenarios/basic.lua examples/switch.lua examples/switch.lua | grep victorious
+	RISC_NUM_THREADS=$i ./dedicated/risc_dedicated scenarios/basic.json examples/switch.lua examples/switch.lua | grep victorious
 done
