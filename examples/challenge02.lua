66040f741de4de446ec99f40f0d33954eb84b130
Rich Lane <rlane@club.cc.cmu.edu>
2011-01-10 23:09:29 -0800

    fix acceleration in challenges

--- examples/challenge02.lua
+++ examples/challenge02.lua
@@ -1,4 +1,4 @@
-thrust(math.random()*math.pi*2,1)
+thrust(math.random()*math.pi*2,ships[class].max_acc/10)
 
 while true do
 	yield()
--- examples/challenge03.lua
+++ examples/challenge03.lua
@@ -1,10 +1,11 @@
-thrust(1.57, 10)
-for i = 1,10 do
+local my_ship = ships[class]
+thrust(1.57, my_ship.max_acc)
+for i = 1,32 do
 	yield()
 end
 
 while true do
 	yield()
 	x, y = position()
-	thrust(angle_between(x,y,0,0), 1)
+	thrust(angle_between(x,y,0,0), my_ship.max_acc/3)
 end
