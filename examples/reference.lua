6491431b478931a9f0a004c1fde0c5d23c914b32
Rich Lane <rlane@club.cc.cmu.edu>
2011-05-22 22:28:20 -0700

    replace reference AI

--- examples/reference.lua
+++ examples/reference.lua
@@ -1,341 +1,299 @@
 local my_class = class
 local my_ship = ships[my_class]
 
-if my_class == "fighter" then
-	local i = math.random(1,256)
-	local t = nil
-	local max_target_distance = my_ship.guns.main.velocity*my_ship.guns.main.ttl*1.5
-	local origin = { x = 0, y = 0, vx = 0, vy = 0 }
-	local target_selector = function(k,c) return c:class() ~= "missile" end
-	local follow_target = nil
-	local follow_target_retry = 0
-	local fire_target = nil
-	local fire_target_retry = 0
+-- Target to follow and focus fire on
+local primary_target = nil
+local primary_target_counter = 0
 
-	local function fire_score(c)
-		local x,y = position()
-		if follow_target and c:id() == follow_target:id() then
-			return 0
-		else
-			local a = angle_between(x,y,c:position())
-			return math.abs(angle_diff(heading(), a))
-		end
-	end
+-- Target of opportunity
+local secondary_target = nil
+local secondary_target_counter = 0
 
-	while true do
-		local msg = recv()
-		if msg then
-			--print("fighter msg: " .. msg)
-		end
 
-		clear_debug_lines()
-		local x, y = position()
-		local vx, vy = velocity()
-
-		if not follow_target and follow_target_retry == 16 then
-			local contacts = sensor_contacts{ enemy = true }
-			_, follow_target = pick(contacts, target_selector)
-		elseif not follow_target then
-			follow_target_retry = follow_target_retry + 1
-		else
-			follow_target = sensor_contact(follow_target:id())
-		end
-
-		local follow
-		if follow_target and distance(x,y,0,0) < 3000 then
-			follow = follow_target
-		else
-			follow = nil
-		end
-
-		local follow_x, follow_y
-		local follow_vx, follow_vy
-		if follow == nil then
-			follow_x, follow_y = 0, 0
-			follow_vx, follow_vy = 0, 0
-		else
-			follow_x, follow_y = follow:position()
-			follow_vx, follow_vy = follow:velocity()
-		end
-		debug_square(follow_x, follow_y, 2*my_ship.radius)
-
-		local urgent_target = 
-			      min_by(sensor_contacts{ distance_lt = max_target_distance, class = "torpedo", enemy = true }, fire_score)
-		if urgent_target then fire_target = urgent_target end
-
-		if not fire_target and fire_target_retry >= 16 then
-			fire_target = min_by(sensor_contacts{ distance_lt = max_target_distance, enemy = true }, fire_score)
-			fire_target_retry = 0
-		elseif not fire_target then
-			fire_target_retry = fire_target_retry + 1
-		else
-			fire_target = sensor_contact(fire_target:id())
-		end
-
-		if fire_target then
-			local tx, ty = fire_target:position()
-			local tvx, tvy = fire_target:velocity()
-			debug_diamond(tx, ty, my_ship.radius)
-			local a = lead(x, y, tx, ty, vx, vy, tvx, tvy, my_ship.guns.main.velocity, my_ship.guns.main.ttl)
-			if a then
-				fire("main", a)
-			else
-				fire_target = nil
-			end
-			--debug_box(t.x-1, t.y-1, t.x+1, t.y+1)
-		else
-			--debug_box_off()
-		end
-
-		drive_towards(follow_x, follow_y, my_ship.max_main_acc*3)
-
-		if follow_target and energy() > ships.missile.cost and math.random(50) == 7 then
-			spawn("missile", follow_target:id())
-		end
-
-		yield()
-	end
-elseif my_class == "ion_cannon_frigate" then
-	local main_target = nil
-	local main_target_retry = 0
-	local max_dist = my_ship.guns.main.length
-
+function main_loop()
 	while true do
 		clear_debug_lines()
-
-		if not main_target and main_target_retry == 16 then
-			local x, y = position()
-			local vx, vy = velocity()
-			main_target = sensor_contacts{ class = "ion_cannon_frigate", enemy = true }[1] or
-			              sensor_contacts{ class = "assault_frigate", enemy = true }[1] or
-			              sensor_contacts{ class = "carrier", enemy = true }[1]
-			main_target_retry = 0
-		elseif not main_target then
-			main_target_retry = main_target_retry + 1
-		else
-			main_target = sensor_contact(main_target:id())
-		end
-
-		if main_target then
-			local x, y = position()
-			local tx, ty = main_target:position()
-			local bearing = angle_between(x, y, tx, ty)
-			local da = angle_diff(heading(),bearing)
-			local dist = distance(x, y, tx, ty)
-			debug_square(tx, ty, 2*my_ship.radius)
-			if dist <= max_dist and math.abs(da) < 0.1 then
-				fire("main", heading())
-			end
-			drive_towards(tx, ty, 200);
-		else
-			drive_towards(0, 0, 100);
-		end
-
-		if reaction_mass() < 0.1*my_ship.reaction_mass then
-			local carrier = sensor_contacts{ enemy=false, class="carrier", limit=1 }[1]
-			if carrier then
-				local tx, ty = carrier:position()
-				drive_towards(tx, ty, 50)
-				send("refuel\0" .. id)
-			end
-		end
-
+		do_radio()
+		pick_primary_target()
+		weapons_ai()
+		engines_ai()
+		do_construction()
 		yield()
 	end
-elseif my_class == "assault_frigate" then
-	local i = math.random(1,256)
-	local t = nil
-	local max_target_distance = my_ship.guns.main.velocity*my_ship.guns.main.ttl*1.5
-	local origin = { x = 0, y = 0, vx = 0, vy = 0 }
-	local target_selector = function(k,c) return c:class() ~= "missile" end
-	local follow_target = nil
-	local follow_target_retry = 0
-	local fire_target = nil
-	local fire_target_retry = 0
+end
 
-	local priorities = {
+
+---- Targeting
+
+-- Lower values mean this ship is more likely to engage other ships of that class
+priorities = {
+	fighter = {
+		fighter = 0.5,
+		carrier = 2,
+		ion_cannon_frigate = 1,
+		assault_frigate = 1.5,
+	},
+	carrier = {
 		fighter = 2,
 		carrier = 1.2,
 		ion_cannon_frigate = 1,
 		assault_frigate = 0.5,
-	}
+	},
+	ion_cannon_frigate = {
+		fighter = 10,
+		carrier = 0.7,
+		ion_cannon_frigate = 0.5,
+		assault_frigate = 1.0,
+	},
+	assault_frigate = {
+		fighter = 8,
+		carrier = 1.2,
+		ion_cannon_frigate = 1,
+		assault_frigate = 0.5,
+	},
+}
 
-	local function fire_score(c)
-		local x,y = position()
-		if follow_target and c:id() == follow_target:id() then
-			return 0
-		else
-			local priority = priorities[c:class()] or 1.0
-			return distance(x, y, c:position()) * priority
+my_priorities = priorities[my_class]
+
+function primary_target_scorer(c)
+	local x,y = position()
+	local a = angle_between(x,y,c:position())
+	local class_score = my_priorities[c:class()] or 10.0
+	local heading_score = math.abs(angle_diff(heading(), a))/math.pi
+	local distance_score = clamp(0, 1, math.log(distance(x, y, c:position()))/3)
+	local rand_score = math.random()
+	return 0.1*heading_score +
+	       0.2*distance_score +
+	       0.4*class_score +
+	       0.2*rand_score
+end
+
+-- We will move towards the primary target
+function pick_primary_target()
+	if primary_target then
+		primary_target = sensor_contact(primary_target:id())
+		if primary_target then
+			local tx, ty = primary_target:position()
+			debug_square(tx, ty, 2*my_ship.radius)
+			return
 		end
+		primary_target_counter = 0
 	end
 
-	while true do
-		local msg = recv()
+	if primary_target_counter == 0 then
+		primary_target = min_by(sensor_contacts{ enemy = true }, primary_target_scorer)
+		primary_target_counter = 16
+	else
+		primary_target_counter = primary_target_counter - 1
+	end
+end
 
-		clear_debug_lines()
+function secondary_target_scorer(c)
+	-- Concentrate fire on the primary target
+	if primary_target and c:id() == primary_target:id() then
+		return 0
+	end
+
+	local x,y = position()
+	local a = angle_between(x,y,c:position())
+	local class_score = my_priorities[c:class()] or 10.0
+	local heading_score = math.abs(angle_diff(heading(), a))/math.pi
+	local rand_score = math.random()
+	return 0.6*heading_score +
+	       0.3*class_score +
+	       0.1*rand_score
+end
+
+-- The secondary target needs to be within weapons range
+function pick_secondary_target(max_dist)
+	if secondary_target then
+		secondary_target = sensor_contact(secondary_target:id())
+		if secondary_target then
+			local tx, ty = secondary_target:position()
+			debug_diamond(tx, ty, 2*my_ship.radius)
+			return
+		end
+		secondary_target_counter = 0
+	end
+
+	if secondary_target_counter == 0 then
+		secondary_target = min_by(sensor_contacts{ enemy = true, distance_lt = max_dist }, secondary_target_scorer)
+		secondary_target_counter = 16
+	else
+		secondary_target_counter = secondary_target_counter - 1
+	end
+end
+
+
+---- Handle radio messages
+
+function do_radio()
+	local msg = recv()
+	while msg do
+		local cmd, arg = msg:match("(%a+)%z(.+)")
+
+		-- Send some reaction mass to the requesting ship
+		if cmd == "refuel" and my_ship.guns.refuel then
+			local refuel_target_id = arg
+			local refuel_range = my_ship.guns.refuel.velocity*my_ship.guns.refuel.ttl
+			local refuel_target = sensor_contact(refuel_target_id)
+			if refuel_target then
+				fire_at_contact("refuel", refuel_target)
+			end
+		end
+
+		msg = recv()
+	end
+end
+
+
+---- Build ships
+
+function do_construction()
+	-- Only carriers can build things other than missiles
+	if my_class ~= "carrier" then
+		return
+	end
+
+	if energy() > my_ship.energy.limit*0.1 and chance(3) then
+		spawn("fighter", "")
+	end
+end
+
+
+---- Weapons AI
+
+weapons = {}
+
+function weapons.carrier()
+	point_defense("laser")
+	pick_secondary_target(2e3)
+
+	if primary_target and chance(3) then
+		spawn("torpedo", primary_target:id())
+	end
+
+	if secondary_target and chance(1) then
+		spawn("missile", secondary_target:id())
+	end
+end
+
+function weapons.assault_frigate()
+	point_defense("laser")
+	pick_secondary_target(my_ship.guns.main.velocity*my_ship.guns.main.ttl*1.5)
+
+	if primary_target and chance(1) then
+		spawn("missile", primary_target:id())
+	end
+
+	if secondary_target then
+		fire_at_contact("main", secondary_target)
+	end
+end
+
+function weapons.ion_cannon_frigate()
+	if primary_target then
 		local x, y = position()
-		local vx, vy = velocity()
-
-		if not follow_target and follow_target_retry == 16 then
-			local contacts = sensor_contacts{ enemy = true }
-			_, follow_target = pick(contacts, target_selector)
-		elseif not follow_target then
-			follow_target_retry = follow_target_retry + 1
-		else
-			follow_target = sensor_contact(follow_target:id())
+		local tx, ty = primary_target:position()
+		local bearing = angle_between(x, y, tx, ty)
+		local da = angle_diff(heading(),bearing)
+		local dist = distance(x, y, tx, ty)
+		if dist <= my_ship.guns.main.length and math.abs(da) < 0.1 then
+			fire("main", heading())
 		end
-
-		local follow
-		if follow_target and distance(x,y,0,0) < 3000 then
-			follow = follow_target
-		else
-			follow = nil
-		end
-
-		local follow_x, follow_y
-		local follow_vx, follow_vy
-		if follow == nil then
-			follow_x, follow_y = 0, 0
-			follow_vx, follow_vy = 0, 0
-		else
-			follow_x, follow_y = follow:position()
-			follow_vx, follow_vy = follow:velocity()
-		end
-		debug_square(follow_x, follow_y, 2*my_ship.radius)
-
-		if not fire_target and fire_target_retry >= 16 then
-			fire_target = min_by(sensor_contacts{ distance_lt = max_target_distance, enemy = true }, fire_score)
-			fire_target_retry = 0
-		elseif not fire_target then
-			fire_target_retry = fire_target_retry + 1
-		else
-			fire_target = sensor_contact(fire_target:id())
-		end
-
-		if fire_target then
-			local tx, ty = fire_target:position()
-			local tvx, tvy = fire_target:velocity()
-			debug_diamond(tx, ty, my_ship.radius)
-			local a = lead(x, y, tx, ty, vx, vy, tvx, tvy, my_ship.guns.main.velocity, my_ship.guns.main.ttl)
-			if a then
-				fire("main", a)
-			else
-				fire_target = nil
-			end
-			--debug_box(t.x-1, t.y-1, t.x+1, t.y+1)
-		else
-			--debug_box_off()
-		end
-
-		local range = my_ship.guns.laser.length
-		local laser_target = sensor_contacts{ distance_lt = range, enemy = true, class = "torpedo", limit = 1 }[1] or
-		                     sensor_contacts{ distance_lt = range, enemy = true, class = "missile", limit = 1 }[1]
-
-		if not laser_target and energy() > 0.1*my_ship.energy.limit then
-			laser_target = sensor_contacts{ distance_lt = range, enemy = true, limit = 1 }[1]
-		end
-
-		if laser_target then
-			local x, y = position()
-			local tx, ty = laser_target:position()
-			local a = angle_between(x, y, tx, ty)
-			fire("laser", a)
-		end
-
-		drive_towards(follow_x, follow_y, my_ship.max_main_acc*3)
-
-		if follow_target and energy() > ships.missile.cost and math.random(50) == 7 then
-			spawn("missile", follow_target:id())
-		end
-
-		if reaction_mass() < 0.1*my_ship.reaction_mass then
-			local carrier = sensor_contacts{ enemy=false, class="carrier", limit=1 }[1]
-			if carrier then
-				local tx, ty = carrier:position()
-				drive_towards(tx, ty, 50)
-				send("refuel\0" .. id)
-			end
-		end
-
-		yield()
 	end
-elseif my_class == "carrier" then
-	local i = 0
-	local t = nil
-	local main_target = nil
+end
 
-	while true do
-		clear_debug_lines()
+function weapons.fighter()
+	pick_secondary_target(my_ship.guns.main.velocity*my_ship.guns.main.ttl*1.2)
 
-		local msg = recv()
-		while msg do
-			--print("carrier msg: " .. msg)
-			cmd, arg = msg:match("(%a+)%z(.+)")
-			if cmd == "refuel" and my_ship.guns.refuel then
-				local refuel_target_id = arg
-				local refuel_range = my_ship.guns.refuel.velocity*my_ship.guns.refuel.ttl
-				local refuel_target = sensor_contact(refuel_target_id)
-				if refuel_target then
-					local x, y = position()
-					local vx, vy = velocity()
-					local tx, ty = refuel_target:position()
-					local tvx, tvy = refuel_target:velocity()
-					debug_diamond(tx, ty, my_ship.radius)
-					local a = lead(x, y, tx, ty, vx, vy, tvx, tvy, my_ship.guns.refuel.velocity, my_ship.guns.refuel.ttl)
-					if a then
-						fire("refuel", a)
-					end
-				end
-			end
-			msg = recv()
-		end
-
-		local range = my_ship.guns.main.length
-		main_target = sensor_contacts{ distance_lt = range, enemy = true, class = "torpedo", limit = 1 }[1] or
-		              sensor_contacts{ distance_lt = range, enemy = true, class = "missile", limit = 1 }[1]
-
-		if not main_target and energy() > 0.1*my_ship.energy.limit then
-			main_target = sensor_contacts{ distance_lt = range, enemy = true, limit = 1 }[1]
-		end
-
-		if main_target then
-			local x, y = position()
-			local tx, ty = main_target:position()
-			local a = angle_between(x, y, tx, ty)
-			if a then
-				fire("main", a)
-			else
-				main_target = nil
-			end
-		end
-
-		if math.random(1,1000) == 5 then
-			send("hello")
-		end
-
-		if math.random(1,100) == 7 then
-			local target_selector = function(k,c) return true end
-			local _, t = pick(sensor_contacts{ class = "carrier", enemy = true }, target_selector)
-			if t then
-				spawn("torpedo", t:id())
-			end
-		end
-
-		if math.random(50) == 7 then
-			local target_selector = function(k,c) return c:class() ~= "missile" end
-			local _, t = pick(sensor_contacts{ enemy = true }, target_selector)
-			if t then
-				spawn("missile", t:id())
-			end
-		end
-
-		if energy() > 100 and math.random(50) == 7 then
-			spawn("fighter", "")
-		end
-
-		yield()
+	if primary_target and chance(2) then
+		spawn("missile", primary_target:id())
 	end
-elseif my_class == "torpedo" or my_class == "missile" then
+
+	if secondary_target then
+		fire_at_contact("main", secondary_target)
+	end
+end
+
+-- Fire short-range defensive weapon at missiles
+function point_defense(gun_name)
+	local range = my_ship.guns[gun_name].length
+	local t = sensor_contacts{ distance_lt = range, enemy = true, class = "torpedo", limit = 1 }[1] or
+	          sensor_contacts{ distance_lt = range, enemy = true, class = "missile", limit = 1 }[1]
+
+	-- Shoot at any enemy if we have spare energy
+	if not t and energy() > 0.1*my_ship.energy.limit then
+		t = sensor_contacts{ distance_lt = range, enemy = true, limit = 1 }[1]
+	end
+
+	if t then
+		fire_at_contact(gun_name, t)
+	end
+end
+
+
+---- Engines AI
+
+engines = {}
+
+function engines.carrier()
+end
+
+function engines.assault_frigate()
+	move_to_primary_target(100)
+	if reaction_mass() < 0.1*my_ship.reaction_mass then move_to_carrier() end
+	if reaction_mass() < 0.01*my_ship.reaction_mass then request_refuel() end
+end
+
+function engines.ion_cannon_frigate()
+	move_to_primary_target(100)
+	if reaction_mass() < 0.1*my_ship.reaction_mass then move_to_carrier() end
+	if reaction_mass() < 0.01*my_ship.reaction_mass then request_refuel() end
+end
+
+function engines.fighter()
+	move_to_primary_target(300)
+	if reaction_mass() < 0.01*my_ship.reaction_mass then explode() end
+end
+
+
+---- Utility functions
+
+-- Send a radio message telling all carriers to send us reaction mass
+function request_refuel()
+	send("refuel\0" .. id)
+end
+
+-- Move to a carrier
+function move_to_carrier()
+	local carrier = sensor_contacts{ enemy=false, class="carrier", limit=1 }[1]
+	if carrier then
+		local tx, ty = carrier:position()
+		drive_towards(tx, ty, 50)
+	end
+end
+
+-- Move to the primary target, or if none then the origin
+function move_to_primary_target(speed)
+	if primary_target then
+		local tx, ty = primary_target:position()
+		drive_towards(tx, ty, speed)
+	else
+		drive_towards(0, 0, speed/2)
+	end
+end
+
+
+---- Configure AI for this ship's class
+
+weapons_ai = weapons[my_class]
+engines_ai = engines[my_class]
+
+---- Run AI
+
+if my_class == "missile" or my_class == "torpedo" then
 	standard_missile_ai()
+else
+	main_loop()
 end
