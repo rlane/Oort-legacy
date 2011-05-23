96ac60f8ecf5b0b13f8c5386f73d31e60ea2348f
Rich Lane <rlane@club.cc.cmu.edu>
2011-05-22 22:26:43 -0700

    do 100 iterations for local tournament

--- devel/run-local-tournament.sh
+++ devel/run-local-tournament.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 DB=local-tournament.yaml
-ITERS=20
+ITERS=100
 export RUBYLIB=tournament/lib
 
 rm -f $DB
@@ -10,6 +10,6 @@ do
 	./tournament/bin/oort-tournament-register -D $DB $X $X
 done
 
-./tournament/bin/oort-tournament -D $DB --iters  $ITERS
+./tournament/bin/oort-tournament -D $DB --iters $ITERS
 
 ./tournament/bin/oort-tournament-report -D $DB
