d2cb90f2edf244f47235245bd433eec81544fca8
Rich Lane <rlane@club.cc.cmu.edu>
2011-09-14 00:28:11 -0700

    use local vala

--- devel/build-linux.sh
+++ devel/build-linux.sh
@@ -3,7 +3,7 @@ set -e
 git clean -f -X -d
 (cd luajit && git clean -f -X -d)
 ./autogen.sh
-./configure --with-valgrind
+./configure --with-valgrind VALAC="/data/pkg/vala-0.13.4/bin/valac -g"
 make all dist check
 tag=`git log --pretty=format:%h -1`
 cp oort-0.1.0.tar.bz2 oort-$tag.tar.bz2
