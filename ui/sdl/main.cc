7e1f66cd20af32a42e06bd8f75fbe23cc689ab11
Rich Lane <rlane@club.cc.cmu.edu>
2011-10-20 19:40:35 -0700

    sdl: basic event loop

--- ui/sdl/main.cc
+++ ui/sdl/main.cc
@@ -13,6 +13,28 @@ using glm::vec2;
 using glm::dvec2;
 using std::make_shared;
 
+static bool running = true;
+
+static void handle_sdl_event(SDL_Event &event)
+{
+	switch(event.type){
+		case SDL_KEYDOWN:
+			switch(event.key.keysym.sym) {
+				case SDLK_ESCAPE:
+					running = false;
+					break;
+				default:
+					break;
+			}
+			break;
+		case SDL_KEYUP:
+			break;
+		case SDL_QUIT:
+			running = false;
+			break;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -23,8 +45,8 @@ int main(int argc, char **argv)
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
 	SDL_Surface *screen = SDL_SetVideoMode(1600, 900, 16, SDL_OPENGL | SDL_FULLSCREEN);
+	glViewport(0, 0, 1600, 900);
 
-	auto a = new vec2(1.0f,2.0f);
 	dvec2 b(2.0f,3.0f);
 	auto ship = make_shared<Oort::Ship>();
 	ship->physics.v = dvec2(2.0, 3.0);
@@ -32,10 +54,26 @@ int main(int argc, char **argv)
 	std::cout << "position:" << glm::to_string(ship->physics.p) << std::endl;
 	auto game = make_shared<Oort::Game>();
 	game->ships.push_back(make_shared<Oort::Ship>());
-	BOOST_FOREACH(auto ship, game->ships) {
-		ship->physics.v = dvec2(1.0, 1.0);
-		ship->physics.tick(1.0/32);
-		std::cout << "position:" << glm::to_string(ship->physics.p) << std::endl;
+
+	SDL_Event event;
+	
+	while (running) {
+		while(SDL_PollEvent(&event)) {
+			handle_sdl_event(event);
+		}
+
+		glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		BOOST_FOREACH(auto ship, game->ships) {
+			ship->physics.v = dvec2(1.0, 1.0);
+			ship->physics.tick(1.0/32);
+			std::cout << "position:" << glm::to_string(ship->physics.p) << std::endl;
+		}
+
+		SDL_GL_SwapBuffers();
+		usleep(31250);
 	}
+
 	return 0;
 }
