34ae6d22d30167a399aec2caac3c060145198c81
Rich Lane <rlane@club.cc.cmu.edu>
2011-10-31 23:15:59 -0700

    use scenario in headless ui

--- ui/headless/main.cc
+++ ui/headless/main.cc
@@ -9,6 +9,7 @@
 #include "sim/ship.h"
 #include "sim/team.h"
 #include "sim/game.h"
+#include "sim/scenario.h"
 
 using glm::vec2;
 using glm::dvec2;
@@ -17,18 +18,12 @@ using std::make_shared;
 namespace Oort {
 
 int main(int argc, char **argv) {
-	dvec2 b(2.0f, 3.0f);
-	auto red_team = make_shared<Team>("red", glm::vec3(1, 0, 0));
-	auto ship = make_shared<Oort::Ship>(red_team);
-	ship->physics.v = dvec2(2.0, 3.0);
-	ship->physics.tick(1.0/32);
-	log("position: %s\n", glm::to_string(ship->physics.p).c_str());
 	auto game = make_shared<Oort::Game>();
-	game->ships.push_back(make_shared<Oort::Ship>(red_team));
+	Scenario scn;
+	scn.setup(*game);
 	BOOST_FOREACH(auto ship, game->ships) {
-		ship->physics.v = dvec2(1.0, 1.0);
+		ship->tick();
 		ship->physics.tick(1.0/32);
-		log("position: %s\n", glm::to_string(ship->physics.p).c_str());
 	}
 	return 0;
 }
