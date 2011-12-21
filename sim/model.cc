c9b1d3b67345d1862d22e4e2a8162036be5e9c30
Rich Lane <rlane@club.cc.cmu.edu>
2011-12-21 06:58:26 -0800

    load model from json

--- sim/model.cc
+++ sim/model.cc
@@ -1,15 +1,36 @@
 #include "sim/model.h"
 
 #include <boost/foreach.hpp>
+#include "json_spirit_reader_template.h"
+#include "json_spirit_reader.h"
+#include "common/resources.h"
 
 namespace Oort {
 
+static Shape read_shape(json_spirit::mArray &vertices) {
+	Shape s;
+	BOOST_FOREACH(json_spirit::mValue &e, vertices) {
+		json_spirit::mObject obj = e.get_obj();
+		float x = float(obj["x"].get_real());
+		float y = float(obj["y"].get_real());
+		s.vertices.push_back(glm::vec2(x, y) * 10.0f);
+	}
+	return s;
+}
+
 Model *Model::load(std::string name) {
+	auto data = load_resource("models/" + name + ".json");
+	json_spirit::mValue value;
+	json_spirit::read_string(data, value);
+	json_spirit::mObject &obj = value.get_obj();
+
 	auto model = new Model;
-	model->name = "fighter";
-	model->shapes.push_back(Shape());
-	model->shapes[0].vertices = { glm::vec2(-0.7, -0.71), glm::vec2(1, 0), glm::vec2(-0.7, 0.71) };
-	BOOST_FOREACH(glm::vec2 &v, model->shapes[0].vertices) { v *= 10; }
+	model->name = obj.find("name")->second.get_str();
+	json_spirit::mArray shapes = obj.find("shapes")->second.get_array();
+	BOOST_FOREACH(json_spirit::mValue &e, shapes) {
+		model->shapes.push_back(read_shape(e.get_array()));
+	}
+	
 	model->collision_shape = model->shapes[0];
 	return model;
 }
