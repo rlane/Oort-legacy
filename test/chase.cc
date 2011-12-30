ffe9846022739fb8ffc3d922e4f9f85b622dfe87
Rich Lane <rlane@club.cc.cmu.edu>
2011-12-30 10:23:42 -0800

    make chase target AI more realistic and shoot missiles at it

--- test/chase.cc
+++ test/chase.cc
@@ -13,15 +13,20 @@ public:
 			return;
 		}
 
-		const GunDef &gun = ship.klass.guns[0];
-		auto a = lead(ship.get_position(), target->get_position(),
-									ship.get_velocity(), target->get_velocity(),
-									gun.velocity, gun.ttl);
-		if (!isnan(a)) {
-			ship.fire_gun(0, a);
+		if (&ship.klass == fighter.get()) {
+			const GunDef &gun = ship.klass.guns[0];
+			auto a = lead(ship.get_position(), target->get_position(),
+			              ship.get_velocity(), target->get_velocity(),
+			              gun.velocity, gun.ttl);
+			if (!isnan(a)) {
+				ship.fire_gun(0, a);
+			}
+			if (ship.game->ticks % 32 == 0) {
+				ship.fire_missile(target);
+			}
 		}
 
-		drive_towards(ship, target->get_position(), 100);
+		drive_towards(ship, target->get_position(), ship.klass.max_main_acc*2);
 	}
 };
 
@@ -29,20 +34,26 @@ class TargetAI : public CxxAI {
 public:
 	int countdown;
 	boost::random::mt19937 prng;
-	boost::random::normal_distribution<> v_dist;
 
 	TargetAI(Ship &ship)
 	  : CxxAI(ship),
 	    countdown(0),
-	    prng(42),
-	    v_dist(0.0, 300.0) {}
+	    prng(42) {}
 
 	void tick() {
+		if (length(ship.get_position()) > 10000) {
+			drive_towards(ship, vec2(0, 0), ship.klass.max_main_acc*2);
+			return;
+		}
+
 		if (countdown == 0) {
+			boost::random::normal_distribution<> main_dist(0, ship.klass.max_main_acc/3);
+			boost::random::normal_distribution<> lateral_dist(0, ship.klass.max_lateral_acc/3);
+			boost::uniform_real<> heading_dist(0, 2*M_PI);
 			countdown = rand() % 100;
-			auto v = vec2(v_dist(prng), v_dist(prng));
-			ship.set_velocity(v);
-			ship.set_heading(radians(orientedAngle(vec2(1,0), normalize(v))));
+			ship.acc_main(main_dist(prng));
+			ship.acc_lateral(lateral_dist(prng));
+			ship.set_heading(heading_dist(prng));
 		} else {
 			countdown--;
 		}
@@ -52,11 +63,20 @@ public:
 class ChaseTest : public Test {
 public:
 	weak_ptr<Ship> target_weak;
+	unique_ptr<ShipClass> speedy;
 
 	ChaseTest() {
 		{
+			ShipClassDef def = *fighter;
+			def.name = "speedy";
+			def.max_main_acc = 1000;
+			def.max_lateral_acc = 500;
+			speedy = unique_ptr<ShipClass>(new ShipClass(def));
+		}
+
+		{
 			auto team = make_shared<Team>("red", CxxAI::factory<TargetAI>(), vec3(1, 0, 0));
-			auto s = make_shared<Ship>(this, *fighter, team);
+			auto s = make_shared<Ship>(this, *speedy, team);
 			s->set_position(vec2(1500, 0));
 			ships.push_back(s);
 			target_weak = s;
