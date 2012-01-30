ebcc337516d5b66dcbe69c0a65f9b7f4c8d46b6c
Rich Lane <rlane@club.cc.cmu.edu>
2012-01-29 16:09:53 -0800

    hardcode missile test scenario

--- test/missile.cc
+++ test/missile.cc
@@ -24,32 +24,15 @@ public:
 
 class MissileTest : public SimpleTest {
 public:
-	weak_ptr<Ship> shipB;
-
-	MissileTest() {
-		auto blue = make_shared<Team>("blue", CxxAI::factory<MissileAI>(), vec3(0, 0, 1));
-		auto red = make_shared<Team>("red", CxxAI::factory<CxxAI>(), vec3(1, 0, 0));
-
-		{
-			auto tmpA = make_shared<Ship>(&*game, *fighter, blue);
-			tmpA->set_position(vec2(0, 0));
-			tmpA->set_heading(0);
-			tmpA->set_velocity(vec2(0,0));
-			game->ships.push_back(tmpA);
-		}
-
-		{
-			auto tmpB = make_shared<Ship>(&*game, *ion_cannon_frigate, red);
-			tmpB->set_position(vec2(500, 0));
-			tmpB->set_heading(M_PI/2);
-			tmpB->set_velocity(vec2(0,0));
-			game->ships.push_back(tmpB);
-			shipB = tmpB;
-		}
+	MissileTest()
+		: SimpleTest(Scenario::load("test/missile.json"),
+				         { CxxAI::factory<MissileAI>(), CxxAI::factory<CxxAI>() })
+	{
 	}
 
 	void after_tick() {
-		if (shipB.expired()) {
+		Team *winner;
+		if (game->ships.empty() || game->check_victory(winner)) {
 			finished = true;
 		}
 	}
